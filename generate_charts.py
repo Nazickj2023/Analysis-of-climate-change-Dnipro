cad91a087829bfd87f217fa33ac9859d928be87f
Nazickj2023 <158591875+Nazickj2023@users.noreply.github.com>
2024-11-15 12:59:05 +0200

    commit

--- generate_charts.py
+++ generate_charts.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 
 
 def load_data(file_path):
-    data = pd.read_csv(file_path, parse_dates=['date'])  # Чтение с преобразованием дат
+    data = pd.read_csv(file_path, parse_dates=['date'])  
     return data
 
 
 def generate_temperature_chart(data):
     plt.figure(figsize=(10, 6))
-    plt.plot(data['date'], data['tavg'], marker='o', color='red', label='Средняя температура (°C)')
-    plt.title('Изменение средней температуры')
+    plt.plot(data['date'], data['tavg'], marker='o', color='red', label='Середня температура (°C)')
+    plt.title('Зміна середньої температури')
     plt.xlabel('Дата')
     plt.ylabel('Температура (°C)')
     plt.grid(True)
@@ -21,10 +21,10 @@ def generate_temperature_chart(data):
 
 def generate_rainfall_chart(data):
     plt.figure(figsize=(10, 6))
-    plt.plot(data['date'], data['prcp'], marker='o', color='blue', label='Осадки (мм)')
-    plt.title('Изменение уровня осадков')
+    plt.plot(data['date'], data['prcp'], marker='o', color='blue', label='Опади (мм)')
+    plt.title('Зміна рівня опадів')
     plt.xlabel('Дата')
-    plt.ylabel('Осадки (мм)')
+    plt.ylabel('Опади (мм)')
     plt.grid(True)
     plt.legend()
     plt.savefig('static/rainfall_chart.png')
@@ -37,4 +37,4 @@ data = load_data('static/export.csv')
 generate_temperature_chart(data)
 generate_rainfall_chart(data)
 
-print("Графики сгенерированы на основе реальных данных.")
+print("Графіки згенеровані з урахуванням реальних даних")
